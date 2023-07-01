96f4f4417207653e47ae506973cebc7a25e8fd7c
Tomasmusic <135050579+Tomasmusic@users.noreply.github.com>
2023-07-01 14:01:56 +0000

    generador de dominio ok

--- src/app.js
+++ src/app.js
@@ -9,3 +9,15 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+var pronoun = ["the", "our"];
+var adj = ["great", "big"];
+var noun = ["jogger", "racoon"];
+
+for (let i = 0; i < pronoun.length; i++) {
+  for (let a = 0; a < adj.length; a++) {
+    for (let b = 0; b < noun.length; b++) {
+      console.log(`${pronoun[i]}${adj[a]}${noun[b]}.com`);
+    }
+  }
+}
